12c242f51f44213d84e6e7736a13951b92902ca4
Anthony DiSpirito <adispirito@case.edu>
2021-09-13 16:37:16 -0400

    In-class exercise function complete; Passes init unit tests
    Function to find y for in-class exercise is complete and passes initial unit tests. Still need to add unit tests for the subfunctions (i.e., modular functions).

---
+++ unit_testing/class_exercise/coordinate_plane.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+
+def find_y_for_x_on_line(plane_coord: tuple, x: Union[int, float]):
+    point_0 = plane_coord[0]
+    point_1 = plane_coord[1]
+    m = find_slope(point_0, point_1)
+    x0 = point_0[0]
+    y0 = point_0[1]
+    b = find_b(x0, y0, m)
+    y = line_equation(x, m, b)
+    return y
+
+
+def find_slope(p0, p1):
+    x0 = p0[0]
+    y0 = p0[1]
+    x1 = p1[0]
+    y1 = p1[1]
+    m = (y1 - y0)/(x1 - x0)
+    return m
+
+
+def find_b(x0, y0, m):
+    b = y0 - x0 * m
+    return b
+
+
+def line_equation(x, m, b):
+    y = m * x + b
+    return y
