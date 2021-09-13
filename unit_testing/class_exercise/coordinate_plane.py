e392813f44b793d46de16eca29c1d95736eed693
Anthony DiSpirito <adispirito@case.edu>
2021-09-13 16:57:51 -0400

    Added final unit tests for modular funcs
    Added the final unit tests for my modular functions, and everything seems to pass my unit tests and the pycodestyle test.

--- unit_testing/class_exercise/coordinate_plane.py
+++ unit_testing/class_exercise/coordinate_plane.py
@@ -12,7 +12,7 @@ def find_y_for_x_on_line(plane_coord: tuple, x: Union[int, float]):
     return y
 
 
-def find_slope(p0, p1):
+def find_slope(p0: tuple, p1: tuple):
     x0 = p0[0]
     y0 = p0[1]
     x1 = p1[0]
@@ -21,11 +21,13 @@ def find_slope(p0, p1):
     return m
 
 
-def find_b(x0, y0, m):
+def find_b(x0: Union[int, float], y0: Union[int, float], m: Union[int, float]):
     b = y0 - x0 * m
     return b
 
 
-def line_equation(x, m, b):
+def line_equation(x: Union[int, float],
+                  m: Union[int, float],
+                  b: Union[int, float]):
     y = m * x + b
     return y
--- unit_testing/class_exercise/test_coordinate_plane.py
+++ unit_testing/class_exercise/test_coordinate_plane.py
@@ -9,7 +9,49 @@ import pytest
         (((0, 1), (1, 0)), 0.5, 0.5),
     ],
 )
-def test_coordinate_plane(plane_coord, x, expected):
+def test_coordinate_plane_find_y_for_x_on_line(plane_coord, x, expected):
     from coordinate_plane import find_y_for_x_on_line
     answer = find_y_for_x_on_line(plane_coord, x)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "p0, p1, expected",
+    [
+        ((0, 1), (1, 0), -1),
+        ((0, 0), (1, 1), 1),
+        ((0, 0), (1, 2), 2),
+    ],
+)
+def test_coordinate_plane_find_slope(p0, p1, expected):
+    from coordinate_plane import find_slope
+    answer = find_slope(p0, p1)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "x0, y0, m, expected",
+    [
+        (0, 0, 1, 0),
+        (2, 2, 2, -2),
+        (2, 5, 4, -3),
+    ],
+)
+def test_coordinate_plane_find_b(x0, y0, m, expected):
+    from coordinate_plane import find_b
+    answer = find_b(x0, y0, m)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "x, m, b, expected",
+    [
+        (0, 1, 0, 0),
+        (3, 4, -3, 9),
+        (3, 7, -20, 1),
+    ],
+)
+def test_coordinate_plane_line_equation(x, m, b, expected):
+    from coordinate_plane import line_equation
+    answer = line_equation(x, m, b)
     assert answer == expected
