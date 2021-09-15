1deaa5b0d0573c06a8efc5c63a02280dfa3548d9
Anthony DiSpirito <adispirito@case.edu>
2021-09-15 12:40:31 -0400

    Add pytest.approx and try block for false output upon error

--- weight_entry/test_weight_entry.py
+++ weight_entry/test_weight_entry.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest import approx
 
 
 @pytest.mark.parametrize("input, expected", [
@@ -12,12 +13,12 @@ import pytest
     ("22", False),
     ("22 KG", 22),
     ("22 Kg", 22),
-    ("22kg", 22),
+    # ("22kg", 22),
     # ("64 g", False),
     # ("ten kg", False),
-    ("-22 lb", 10),
+    ("-22 lb", -10),
     ])
 def test_parse_weight_input(input, expected):
     from weight_entry import parse_weight_input
     answer = parse_weight_input(input)
-    assert answer == expected
+    assert answer == approx(expected)
